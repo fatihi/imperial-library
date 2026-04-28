597ff40d5dc0e59ef1940f1bf65e9ea383b63ae9
Fatih Inan <fatihinan@gmail.com>
2026-04-28 07:27:13 +0200

    Registered alias superuser commands in registry

--- src/Structures/commands.js
+++ src/Structures/commands.js
@@ -9,6 +9,10 @@
 import Help from "./../Commands/help.js";
 import About from "./../Commands/about.js";
 
+import AliasAdd from "./../Commands/Superuser/aliasAdd.js";
+import AliasRemove from "./../Commands/Superuser/aliasRemove.js";
+import AliasView from "./../Commands/Superuser/aliasView.js";
+
 import WhitelistAddServer from "../Commands/Superuser/whitelistServerAdd.js";
 import WhitelistRemoveServer from "../Commands/Superuser/whitelistServerRemove.js";
 import WhitelistViewServers from "../Commands/Superuser/whitelistServerView.js";
@@ -19,6 +23,10 @@ export async function init(client) {
   const commands = [
     Help,
     About,
+
+    AliasAdd,
+    AliasRemove,
+    AliasView,
 
     WhitelistAddServer,
     WhitelistRemoveServer,
