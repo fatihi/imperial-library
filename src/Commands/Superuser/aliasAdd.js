2c4774167776dee77e21ac6a355691790cb06702
Fatih Inan <fatihinan@gmail.com>
2026-04-28 07:25:32 +0200

    Added /alias_add superuser command

---
+++ src/Commands/Superuser/aliasAdd.js
@@ -0,0 +1,95 @@
+/**
+ * A superuser command for adding card aliases.
+ *
+ * @file   This files defines the aliasAdd command module.
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+
+import {
+  EmbedBuilder,
+  PermissionFlagsBits,
+  SlashCommandBuilder,
+} from "discord.js";
+import { addAlias, saveAliases } from "../../Aliases/aliases.js";
+import {
+  getAllNormalisedNames,
+  denormaliseCardName,
+} from "../../EmeraldLegacy/api.js";
+import { normalise } from "../../Utility/text.js";
+
+///////////////////////////////////////////////////////////////////////////////
+
+const data = new SlashCommandBuilder()
+  .setName("alias_add")
+  .setDescription("adds an alias for a given card")
+  .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+  .addStringOption((option) =>
+    option
+      .setName("alias")
+      .setDescription("the alias to map to a card")
+      .setRequired(true)
+  )
+  .addStringOption((option) =>
+    option
+      .setName("card")
+      .setDescription("the card the alias will map to")
+      .setRequired(true)
+      .setAutocomplete(true)
+  )
+  .addBooleanOption((option) =>
+    option
+      .setName("can_group")
+      .setDescription("whether the alias can be a group alias")
+  );
+
+const meta = {
+  hideFromHelp: true,
+};
+
+async function execute(interaction, client) {
+  if (interaction.user.id != process.env.SUPER_USER) {
+    const embed = new EmbedBuilder()
+      .setTitle("Invalid permissions!")
+      .setDescription(
+        `You do not have permission to use this command, but you are seeing it because Discord does not allow any commands to be hidden from administrators.`
+      )
+      .setColor(+process.env.COLOR_ERROR);
+    await interaction.reply({ embeds: [embed], ephemeral: true });
+    return;
+  }
+
+  const alias = interaction.options.getString("alias");
+  const cardName = interaction.options.getString("card");
+  const canGroup = interaction.options.getBoolean("can_group");
+  const success = addAlias(alias, cardName, canGroup);
+
+  let embed;
+  if (success) {
+    embed = new EmbedBuilder()
+      .setTitle("Alias added!")
+      .setDescription(`\`${alias}\` ⇒ \`${cardName}\``)
+      .setColor(+process.env.COLOR_INFO);
+    saveAliases();
+  } else {
+    embed = new EmbedBuilder()
+      .setTitle("Alias already exists!")
+      .setDescription(`\`${alias}\` is already an alias for a card.`)
+      .setColor(+process.env.COLOR_ERROR);
+  }
+
+  await interaction.reply({ embeds: [embed], ephemeral: true });
+}
+
+async function autocomplete(interaction) {
+  const focusedValue = normalise(interaction.options.getFocused());
+  const validChoices = getAllNormalisedNames()
+    .filter((n) => n.includes(focusedValue))
+    .slice(0, 25)
+    .map((n) => ({ name: denormaliseCardName(n), value: denormaliseCardName(n) }));
+  await interaction.respond(validChoices);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+export default { data, meta, execute, autocomplete };
